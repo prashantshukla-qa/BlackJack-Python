2d61d2e4e59b695d284cea707caf82d632d5d131
Prashant Shukla <prashantshukla@qainfotech.com>
2021-06-13 14:30:57 +0530

    Updating methods in Card and Deck class

--- Card.py
+++ Card.py
@@ -1,11 +1,20 @@
+'''Card class
+    Attributes: Card SUITES, RANKS and Values dictionary
+'''
 SUITES = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 RANKS = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
          'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-VALUES = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10,
+VALUES = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6,
+          'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10,
           'Queen': 10, 'King': 10, 'Ace': 11}
 
 
 class Card:
+    '''Initiate and Get Card Designation'''
 
-    def __init__(self):
-        pass
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
