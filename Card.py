c78983b72d4e651ce97330992c0b0e0f0ab376fb
Prashant Shukla <prashantshukla@qainfotech.com>
2021-06-13 14:59:19 +0530

    Using dataclass to define cards

--- Card.py
+++ Card.py
@@ -1,6 +1,9 @@
+
 '''Card class
     Attributes: Card SUITES, RANKS and Values dictionary
 '''
+from dataclasses import dataclass
+
 SUITES = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 RANKS = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
          'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
@@ -8,13 +11,11 @@ VALUES = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6,
           'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10,
           'Queen': 10, 'King': 10, 'Ace': 11}
 
-
+@dataclass
 class Card:
-    '''Initiate and Get Card Designation'''
-
-    def __init__(self, suit, rank):
-        self.suit = suit
-        self.rank = rank
+    '''Card class'''
+    suit: str
+    rank: str
 
     def __str__(self):
         return self.rank + ' of ' + self.suit
